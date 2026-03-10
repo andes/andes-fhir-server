7bf979f5922cb7ff6017a1f5cdfcc4418516e29d
jrojas <cesarlinux@gmail.com>
2026-03-10 13:25:53 -0300

    chore: refactor de practitioner, separa service de controller

--- src/services/practitioner/practitioner.service.ts
+++ src/services/practitioner/practitioner.service.ts
@@ -1,162 +1,30 @@
-import { ServerError, resolveSchema } from '@asymmetrik/node-fhir-server-core';
-import { Practitioner as fhirPractitioner } from '@andes/fhir';
-import { familyQueryBuilder, tokenQueryBuilder } from './../../utils/querybuilder.util';
-import { setObjectId as objectId } from './../../utils/uid.util';
-var moment = require('moment');
-const ObjectID = require('mongodb').ObjectID
+import { buscarPractitioner, buscarPractitionerId } from './../../controller/practitioner/practitioner';
 
-const { CONSTANTS } = require('./../../constants');
-const globals = require('../../globals');
-
-let getPractitioner = (base_version) => {
-    return resolveSchema(base_version, 'Practitioner');
-};
-
-let buildAndesSearchQuery = (args) => {
-    // Filtros de búsqueda para profesionales
-    let active = args['active'] ? args['active'] : true;
-    let family = args['family'] ? args['family'] : '';
-    let given = args['given'] ? args['given'] : '';
-    let identifier = args['identifier'];
-    let query: any = {};
-
-    query.$and = [];
-    query.$and.push({ profesionalMatriculado: true });
-    if (active === true || active === 'true') {
-        query.$and.push({
-            $or: [
-                { habilitado: true },
-                { habilitado: { $exists: false } }
-            ]
-        });
-    } else {
-        query.$and.push({ habilitado: false });
-    }
-
-    // Si hay filtros de nombre
-    if (family || given) {
-        query.$and.push(...familyQueryBuilder(family + ' ' + given));
-    }
-
-    // Controles de identifier de profesional
-    if (identifier) {
-        let tokenBuilder: any = tokenQueryBuilder(identifier, 'value', 'identifier', false);
-        switch (tokenBuilder.system) {
-            case 'andes.gob.ar':
-                query._id = new ObjectID(tokenBuilder.value);
-                break;
-            case 'andes.gob.ar/matriculaciones':
-                if (tokenBuilder.value.includes('@')) {
-                    /*  Consulta por profesional. Dado un nro de matricula y codigo de carrera de grado o posgrado,
-                        retorna un profesional siempre que esté activo.
-                    */
-                    const [nroMatricula, tipoProfesion] = tokenBuilder.value.split('@');
-                    query['$or'] = [];
-                    query['$or'].push({ 'formacionGrado.matriculacion.matriculaNumero': parseInt(nroMatricula || 0, 10), 'formacionGrado.profesion.codigo': parseInt(tipoProfesion || 0, 10) });
-                    query['$or'].push({ 'formacionPosgrado.matriculacion.matriculaNumero': parseInt(nroMatricula || 0, 10), 'formacionPosgrado.especialidad.codigo.sisa': parseInt(tipoProfesion || 0, 10) });
-                } else {
-                    if (parseInt(tokenBuilder.value)) {
-                        query['$or'] = [];
-                        query['$or'].push({ 'formacionGrado.matriculacion.matriculaNumero': parseInt(tokenBuilder.value || 0, 10) });
-                        query['$or'].push({ 'formacionPosgrado.matriculacion.matriculaNumero': parseInt(tokenBuilder.value || 0, 10) });
-                    }
-                }
-                break;
-            case 'https://seti.afip.gob.ar/padron-puc-constancia-internet/ConsultaConstanciaAction.do':
-                query.cuit = tokenBuilder.value;
-                break;
-            case 'http://www.renaper.gob.ar/dni':
-                query.documento = tokenBuilder.value;
-                break;
-            default:
-                query.documento = tokenBuilder.value;
+async function search(args: any, context: any) {
+    try {
+        let { base_version } = args;
+        if (Object.keys(args).length > 1) {
+            return await buscarPractitioner(base_version, args, context.req);
+        } else {
+            throw { warning: 'Al menos un parámetro de entrada es requerido' };
         }
+    } catch (err) {
+        return err;
     }
-    return query;
-};
-
-let verificarVigencia = (formacion, codigoProfesion, nroMatricula) => {
-    if (formacion.profesion.codigo.toString() === codigoProfesion && formacion.matriculacion[formacion.matriculacion.length - 1].matriculaNumero.toString() === nroMatricula) {
-        if (formacion.matriculacion[formacion.matriculacion.length - 1].fin >= moment().toDate()) {
-            return true;
-        }
-    }
-    return false
 }
 
-export = {
-    search: async (args, context) => {
-        try {
-            let { base_version } = args;
-            if (Object.keys(args).length > 1) {
-                const query = buildAndesSearchQuery(args);
-                const db = globals.get(CONSTANTS.CLIENT_DB);
-                const collection = db.collection(`${CONSTANTS.COLLECTION.PRACTITIONER}`);
-                const Practitioner = getPractitioner(base_version);
-                let practitioners = await collection.find(query).toArray();
-                if (practitioners.length) {
-                    return practitioners.map(prac => new Practitioner(fhirPractitioner.encode(prac)));
-                } else {
-                    return []
-                }
-            } else {
-                throw { warning: 'Al menos un parámetro de entrada es requerido' };
-            }
-        } catch (err) {
-            let message, system, code = '';
-            if (typeof err === 'object') {
-                message = err.message;
-                system = err.system;
-                code = err.code
-            } else {
-                message = err
-            }
-            throw new ServerError(message, {
-                resourceType: "OperationOutcome",
-                issue: [
-                    {
-                        severity: 'error',
-                        code,
-                        diagnostics: message
-                    }
-                ]
-            });
-
-        }
-    },
-    searchById: async (args, context) => {
-        try {
-            let { base_version, id } = args;
-            let Practitioner = getPractitioner(base_version);
-            let db = globals.get(CONSTANTS.CLIENT_DB);
-            let collection = db.collection(`${CONSTANTS.COLLECTION.PRACTITIONER}`);
-            let practitioner = await collection.findOne({ _id: objectId(id) });
-            return practitioner ? new Practitioner(fhirPractitioner.encode(practitioner)) : { notFound: 404 };
-        } catch (err) {
-            let message, system, code = '';
-            if (typeof err === 'object') {
-                message = err.message;
-                system = err.system;
-                code = err.code
-            } else {
-                message = err
-            }
-            throw new ServerError(message, {
-                resourceType: "OperationOutcome",
-                issue: [
-                    {
-                        severity: 'error',
-                        code,
-                        diagnostics: message
-                    }
-                ]
-            });
-        }
+async function searchById(args: any, context: any) {
+    try {
+        let { base_version, id } = args;
+        return await buscarPractitionerId(base_version, id);
+    } catch (err) {
+        return err;
     }
+}
 
+const PractitionerService = {
+    search,
+    searchById
 };
 
-
-
-
-
+export = PractitionerService;
